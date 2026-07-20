dc2a54c1ac1fb8a29379252f33096ab411f59fde
Charlie-robin <charlie.robin.richardson@gmail.com>
2026-07-20 14:41:18 +0100

    Added Two pointers

--- sorting/sorting.benchmark.js
+++ sorting/sorting.benchmark.js
@@ -1,28 +1,41 @@
-import Benchmark from 'benchmark'
-import { bubbleSort } from './bubble.js'
-import { insertionSort } from './insertion.js'
-import { mergeSort } from './merge.js'
-import { quickSort } from './quick.js'
-import { randArray } from '../utils.js'
+import Benchmark from "benchmark";
+import { bubbleSort } from "./bubble.js";
+import { insertionSort } from "./insertion.js";
+import { mergeSort } from "./merge.js";
+import { quickSort } from "./quick.js";
+import { randArray } from "../utils.js";
 
-const arr = randArray(10_000)
+const xl = randArray(10_000);
+const l = randArray(1000);
 
-const suite = new Benchmark.Suite()
+const suite = new Benchmark.Suite();
 
 suite
-  .add('Bubble Sort', () => {
-    bubbleSort([...arr])
+  .add("Bubble Sort 1000", () => {
+    bubbleSort([...l]);
   })
-  .add('Insertion Sort', () => {
-    insertionSort([...arr])
+  .add("Insertion Sort 1000", () => {
+    insertionSort([...l]);
   })
-  .add('Merge Sort', () => {
-    mergeSort([...arr])
+  .add("Merge Sort 1000", () => {
+    mergeSort([...l]);
   })
-  .add('Quick Sort', () => {
-    quickSort([...arr])
+  .add("Quick Sort 1000", () => {
+    quickSort([...l]);
   })
-  .on('cycle', event => {
-    console.log(String(event.target))
+  .add("Bubble Sort 10_000", () => {
+    bubbleSort([...xl]);
   })
-  .run({ async: true })
+  .add("Insertion Sort 10_000", () => {
+    insertionSort([...xl]);
+  })
+  .add("Merge Sort 10_000", () => {
+    mergeSort([...xl]);
+  })
+  .add("Quick Sort 10_000", () => {
+    quickSort([...xl]);
+  })
+  .on("cycle", (event) => {
+    console.log(String(event.target));
+  })
+  .run({ async: true });
--- two-pointers/remove-dupes/removeDupes.js
+++ two-pointers/remove-dupes/removeDupes.js
@@ -1,7 +1,24 @@
-export function removeDupesA(arr) {
-  // TODO
+export function removeDupesA(array) {
+  const result = [];
+
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] !== array[index + 1]) {
+      result.push(array[index]);
+    }
+  }
+
+  return result;
 }
 
-export function removeDupesB(arr) {
-  // TODO
+export function removeDupesB(array) {
+  let write = 1;
+
+  for (let read = 1; read < array.length; read++) {
+    if (array[read] !== array[read - 1]) {
+      array[write] = array[read];
+      write++;
+    }
+  }
+
+  return array.slice(0, write);
 }
