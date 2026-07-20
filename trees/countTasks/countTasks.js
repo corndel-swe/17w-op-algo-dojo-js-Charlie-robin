528b47420680f199ba8d74471c3b41c45bc68990
Charlie-robin <charlie.robin.richardson@gmail.com>
2026-07-20 15:47:20 +0100

    Trees demo done

--- trees/countTasks/countTasks.js
+++ trees/countTasks/countTasks.js
@@ -1,7 +1,23 @@
+
 export function countTasksA(root) {
-  // TODO
+  let count = 0
+  const stack = [root]
+
+  while( stack.length > 0){
+    const ticket = stack.pop()
+    count++
+    ticket.children.forEach(child => {
+      stack.push(child)
+    });
+  }
+
+  return count;
 }
 
 export function countTasksB(node) {
-  // TODO
+  let count = 1;
+  for (let index = 0; index < node.children.length; index++) {
+    count += countTasksB(node.children[index])
+  }
+  return count;
 }
