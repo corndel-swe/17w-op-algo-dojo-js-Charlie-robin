f2cf21112e63c063267010a98c20991b6d776543
Charlie-robin <charlie.robin.richardson@gmail.com>
2026-07-20 16:33:29 +0100

    Shift Zeros

--- two-pointers/shift-zeros/shiftZeros.js
+++ two-pointers/shift-zeros/shiftZeros.js
@@ -1,7 +1,33 @@
 export function shiftZerosA(arr) {
-  // TODO
+  const { nonZeros, zeros } = arr.reduce(
+    (acc, cur) => {
+      if (cur === 0) {
+        acc.zeros.push(cur);
+      } else {
+        acc.nonZeros.push(cur);
+      }
+
+      return acc;
+    },
+    { nonZeros: [], zeros: [] },
+  );
+
+  return nonZeros.concat(zeros);
 }
 
+
 export function shiftZerosB(arr) {
-  // TODO
+  const copy = [...arr];
+  let write = 0;
+
+  for (let index = 0; index < copy.length; index++) {
+    if (copy[index] !== 0) {
+      copy[write] = copy[index];
+      write++;
+    }
+  }
+
+  copy.fill(0, write, copy.length);
+
+  return copy;
 }
